95b97fbd331d18f499741c58749c9b67906b1999
yutooo310 <57031298+yutooo310@users.noreply.github.com>
2020-03-15 17:01:22 +0800

    create likesView

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -6,7 +6,7 @@ export default class Likes {
     addLike(id, title, publisher, image) {
         const like = { id, title, publisher, image};
         this.likes.push(like);
-        return likes;
+        return like;
     }
     
     deletLike (id) {
